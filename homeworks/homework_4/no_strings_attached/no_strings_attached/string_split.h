cb6c5075c57fe0c04da3b7b4472b71c1917a943e
Kausik <kausikp11@gmail.com>
2024-04-19 10:35:53 +0530

    Update string_split.h
    Made to one name space

--- homeworks/homework_4/no_strings_attached/no_strings_attached/string_split.h
+++ homeworks/homework_4/no_strings_attached/no_strings_attached/string_split.h
@@ -5,13 +5,13 @@
 #include <vector>
 
 namespace no_strings_attached {
-namespace string_split {
+//namespace string_split {
 std::vector<std::string> Split(const std::string &str,
                                const std::string &delimiter);
 std::vector<std::string> Split(const std::string &str,
                                const std::string &delimiter,
                                int number_of_chunks_to_keep);
-} // namespace string_split
+//} // namespace string_split
 } // namespace no_strings_attached
 
 #endif /* NO_STRINGS_ATTACHED_STRING_SPLIT_H */
