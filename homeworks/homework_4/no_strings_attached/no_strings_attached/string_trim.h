74b890348a0271adb826d7a5e52ad2a3cf9cf040
Kausik <kausikp11@gmail.com>
2024-04-19 10:43:10 +0530

    Update string_trim.h
    Namespace made common

--- homeworks/homework_4/no_strings_attached/no_strings_attached/string_trim.h
+++ homeworks/homework_4/no_strings_attached/no_strings_attached/string_trim.h
@@ -6,11 +6,11 @@
 
 
 namespace no_strings_attached{
-    namespace string_trim{
+    //namespace string_trim{
         enum class Side { kLeft, kRight, kBoth};
         std::string Trim(const std::string& str, char char_to_trim, Side side);
         std::string Trim(const std::string& str);
-    }
+   // }
 }
 
 #endif /* NO_STRINGS_ATTACHED_STRING_TRIM_H */
