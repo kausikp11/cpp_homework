869b1e91dbfcd13b5e26be8b100ccd9acc66e833
Kausik <kausikp11@gmail.com>
2024-04-19 10:37:21 +0530

    Update string_split_test.cpp
    Name space common

--- homeworks/homework_4/no_strings_attached/no_strings_attached/string_split_test.cpp
+++ homeworks/homework_4/no_strings_attached/no_strings_attached/string_split_test.cpp
@@ -25,10 +25,10 @@ TEST(TestTopic, TrivialEquality) {
 
 TEST(TestTopic,HelloWorldSplit){
     std::vector<std::string> assert_value = {"hello", "world"};
-    EXPECT_EQ(no_strings_attached::string_split::Split("hello world", " "),assert_value)<<"No a problem";
+    EXPECT_EQ(no_strings_attached::Split("hello world", " "),assert_value)<<"No a problem";
 }
 
 //TEST(TestTopic,RandomString){
  //   std::vector<std::string> assert_value = {"", "ab", "ba"};
-  //  EXPECT_EQ(no_strings_attached::string_split::Split("aaabaaba", "aa"),assert_value)<<"No a problem";
+  //  EXPECT_EQ(no_strings_attached::Split("aaabaaba", "aa"),assert_value)<<"No a problem";
 //}
