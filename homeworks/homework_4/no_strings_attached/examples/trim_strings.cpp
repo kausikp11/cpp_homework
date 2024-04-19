e61f6e773f3744a7ae1ae15597b8c680ada7495d
Kausik <kausikp11@gmail.com>
2024-04-19 08:40:21 +0530

    Update trim_strings.cpp
    Added additional space

--- homeworks/homework_4/no_strings_attached/examples/trim_strings.cpp
+++ homeworks/homework_4/no_strings_attached/examples/trim_strings.cpp
@@ -9,7 +9,7 @@ int main() {
   std::string str{};
   std::string ouput_string{};
   std::string output;
-  std::cout << "Example program that trims strings.\nPlease enter a string:\n";
+  std::cout << "Example program that trims strings.\nPlease enter a string:\n\n";
   std::getline(std::cin, str);
   output = no_strings_attached::string_trim::Trim(str,' ',no_strings_attached::string_trim::Side::kBoth);
   ouput_string = "Your trimmed string: '"s + output+"'"s;
