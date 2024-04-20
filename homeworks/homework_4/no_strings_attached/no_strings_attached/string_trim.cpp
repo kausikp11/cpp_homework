0ce8209072fd4d258c73a23c558fa514374c2268
Kausik <kausikp11@gmail.com>
2024-04-20 06:03:00 +0530

    Update string_trim.cpp
    method changed

--- homeworks/homework_4/no_strings_attached/no_strings_attached/string_trim.cpp
+++ homeworks/homework_4/no_strings_attached/no_strings_attached/string_trim.cpp
@@ -5,61 +5,114 @@
 
 namespace no_strings_attached {
 //namespace string_trim {
+    //A function that allows trimming strings from a given direction:
     std::string Trim(const std::string& str, char char_to_trim, Side side){
-        std::string str_original = str;
-        int found = 0;
-        char char_to_trim_original = char_to_trim;
-        switch (side)
-        {
-        case Side::kLeft:
-        while(true){
-            if(str_original[found]==char_to_trim_original){
-                str_original[found] = '\0';
-                found++;
-            }
-            else{
+
+        int next_start = 1; // since char type
+
+        int start_index, end_index, count;
+
+        std::string trim_str_init = str;
+
+        if(trim_str_init.find(char_to_trim) != std::string::npos){
+
+            switch(side){
+
+                case Side::kLeft:
+                    if((start_index = trim_str_init.find_first_of(char_to_trim)) == 0){
+
+                        end_index = trim_str_init.find_first_not_of(char_to_trim,start_index);
+                        count = end_index-start_index;
+                        trim_str_init.replace(start_index, count, "");                 
+                        return trim_str_init;
+
+                    }else{
+                        
+                        return str;
+                    }
                 break;
-            }
-        }
-            break;
-        case Side::kRight:
-        found = str_original.size()-1;
-            while(true){
-            if(str_original[found]==char_to_trim_original){
-                str_original[found] = '\0';
-                found--;
-            }
-            else{
+
+                case Side::kRight:
+                    if((start_index = trim_str_init.find_last_of(char_to_trim)) == trim_str_init.size()-1){
+
+                        end_index = trim_str_init.find_last_not_of(char_to_trim); //find_last_not_of( const basic_string& str,size_type pos = npos )
+                        count = start_index - end_index;
+                        trim_str_init.replace(end_index+next_start, count, "");                 
+                        return trim_str_init;
+
+                    }else{
+                        
+                        return str;
+                    }
                 break;
+
+                case Side::kBoth:
+                    if((start_index = trim_str_init.find_first_of(char_to_trim)) == 0){
+
+                        end_index = trim_str_init.find_first_not_of(char_to_trim,start_index);
+                        count = end_index-start_index;
+                        trim_str_init.replace(start_index, count, "");                 
+                    }
+
+                    if((start_index = trim_str_init.find_last_of(char_to_trim)) == trim_str_init.size()-1){
+
+                        end_index = trim_str_init.find_last_not_of(char_to_trim); //find_last_not_of( const basic_string& str,size_type pos = npos )
+                        count = start_index - end_index;
+                        trim_str_init.replace(end_index+next_start, count, "");                 
+
+                    }
+                    return trim_str_init;
+                break;            
+            
+
             }
+
+        }else{
+            return str;
         }
-        break;
-        case Side::kBoth:
-            while(true){
-            if(str_original[found]==char_to_trim_original){
-                str_original[found] = '\0';
-                found++;
-            }
-            else{
-                break;
-            }
-        }
-        found = str_original.size()-1;
-            while(true){
-            if(str_original[found]==char_to_trim_original){
-                str_original[found] = '\0';
-                found--;
-            }
-            else{
-                break;
-            }
-        }
-            break;
-        }
-        return str_original;
+
+
+
     }
+
+
+
+
+
+    // An overload of this function function that allows trimming spaces from both sides:
     std::string Trim(const std::string& str){
-        return Trim(str,' ',Side::kBoth);
+
+        int next_start = 1; // since space character
+
+        int start_index, end_index, count;
+
+        std::string trim_str_init = str;
+
+        std::string char_to_trim = " "; // space character
+
+        if(trim_str_init.find(char_to_trim) != std::string::npos){
+
+                    if((start_index = trim_str_init.find_first_of(char_to_trim)) == 0){
+
+                        end_index = trim_str_init.find_first_not_of(char_to_trim,start_index);
+                        count = end_index-start_index;
+                        trim_str_init.replace(start_index, count, "");                 
+                    }
+
+                    if((start_index = trim_str_init.find_last_of(char_to_trim)) == trim_str_init.size()-1){
+
+                        end_index = trim_str_init.find_last_not_of(char_to_trim); //find_last_not_of( const basic_string& str,size_type pos = npos )
+                        count = start_index - end_index;
+                        trim_str_init.replace(end_index+next_start, count, "");                 
+
+                    }
+                    return trim_str_init;
+
+        }else{
+            return str;
+        }
+    }
+::kBoth);
     }
 //} // namespace string_trim
 } // namespace no_strings_attached
