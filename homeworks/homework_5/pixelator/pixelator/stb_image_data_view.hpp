6b0bfb01c32e11fc320b6b77baab03e50e269576
kausikp11 <kausikp11@gmail.com>
2024-04-30 22:05:42 +0530

    Initial submission of Homework 5

---
+++ homeworks/homework_5/pixelator/pixelator/stb_image_data_view.hpp
@@ -0,0 +1,38 @@
+#ifndef PIXELATOR_STB_IMAGE_DATA_VIEW_HPP
+#define PIXELATOR_STB_IMAGE_DATA_VIEW_HPP
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+#include "ftxui/screen/color.hpp"
+#include "ftxui/screen/screen.hpp"
+#include <filesystem>
+#include <iostream>
+
+namespace pixelator
+{
+   struct Size
+   {
+      int *row;
+      int *col;
+   };
+   class StbImageDataView
+   {
+   public:
+      StbImageDataView() = default;
+      explicit StbImageDataView(std::filesystem::path image_path);
+
+      pixelator::Size size();
+      int rows();
+      int cols();
+      bool empty();
+      ftxui::Color at(int row, int col);
+      ~StbImageDataView();
+
+   private:
+      unsigned char *image_{nullptr};
+      pixelator::Size size_of_image_{};
+      int *channels_{};
+   };
+} // namespace pixelator
+
+#endif /* PIXELATOR_STB_IMAGE_DATA_VIEW_HPP */
