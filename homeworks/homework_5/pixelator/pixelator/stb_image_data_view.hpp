b3defbccb721f4574f144d5eebc26339224b73c0
kausikp11 <kausikp11@gmail.com>
2024-05-04 10:17:35 +0530

    Updated the stb wrapper

--- homeworks/homework_5/pixelator/pixelator/stb_image_data_view.hpp
+++ homeworks/homework_5/pixelator/pixelator/stb_image_data_view.hpp
@@ -1,95 +1,37 @@
-#pragma once
-#define STB_IMAGE_IMPLEMENTATION
+#ifndef PIXELATOR_STB_IMAGE_DATA_VIEW_HPP
+#define PIXELATOR_STB_IMAGE_DATA_VIEW_HPP
 
-#include "size.hpp"
-#include "stb/stb_image.h"
-#include <ftxui/screen/color.hpp>
-
-#include <iostream>
-#include <string>
 #include <filesystem>
+#include <iostream>
 
-// Some path to an image on disk.
-// const std::filesystem::path image_path{"../../pixelator/test_data/test.png"};
+#include "ftxui/screen/color.hpp"
 
-namespace pixelator
-{
-    class StbImageDataView
-    {
-    private:
-        int cols_;
-        int rows_;
-        int channels_;
-        unsigned char * image_data{nullptr};
-    public:
-        StbImageDataView() : cols_{0}, rows_{0}, channels_{0}, image_data{nullptr} {};
-        StbImageDataView(const std::string& image_path){
-            image_data = stbi_load(image_path.c_str(), &cols_, &rows_, &channels_, 0);
-            if (!image_data) {
-                std::cerr << "Failed to load image data from file: " << image_path << std::endl;
-                rows_ = 0;
-                cols_ = 0;
-                channels_ = 0;
-            }
-        };
-        StbImageDataView(StbImageDataView&& other_image): 
-        rows_{other_image.rows_}, cols_{other_image.cols_}, channels_{other_image.channels_},
-        image_data{other_image.image_data}
-        {            
-            other_image.image_data = nullptr;
-            other_image.rows_ = 0;
-            other_image.cols_ = 0;
-            other_image.channels_ = 0;
-        };
+namespace pixelator {
+struct Size {
+  int rows;
+  int cols;
+};
+class StbImageDataView {
+ public:
+  StbImageDataView();
+  StbImageDataView(std::filesystem::path image_path);
+  StbImageDataView(StbImageDataView&& other_image);
+  StbImageDataView& operator=(StbImageDataView&& other);
 
-        StbImageDataView &operator=(StbImageDataView &&other_image){
-            if (this == &other_image) { return *this; }  // Do not self-assign.
-            if (image_data) stbi_image_free(image_data);
+  pixelator::Size size() const;
+  int rows() const;
+  int cols() const;
+  bool empty() const;
+  const ftxui::Color at(int row, int col) const;
+  ~StbImageDataView();
 
-            image_data = other_image.image_data;
+ private:
+  unsigned char* image_{nullptr};
+  int rows_;
+  int cols_;
+  pixelator::Size size_of_image_{};
+  int channels_;
+};
+}  // namespace pixelator
 
-            rows_ = other_image.rows_;
-            cols_ = other_image.cols_; 
-            channels_ = other_image.channels_;   
-
-            other_image.image_data = nullptr;
-            other_image.rows_ = 0;
-            other_image.cols_ = 0;
-            other_image.channels_ = 0;
-
-            return *this;
-        };
-
-        bool empty() const {
-            if(rows_ == 0 && cols_ == 0){
-                return true;
-            }
-            return false;
-        }
-        int rows() const {
-            return rows_;
-        }
-
-        int cols() const {
-            return cols_;
-        }
-        
-        const ftxui::Color at(int row, int col) const {
-            const auto index{channels_ * (row * cols_ + col)};
-            const ftxui::Color color{image_data[index], image_data[index + 1], image_data[index + 2]};
-            return color;
-        }
-
-        Size size() const {
-            return Size{rows_,cols_};
-        }
-
-        ~StbImageDataView() {
-            if (image_data != nullptr)
-            {
-                stbi_image_free(image_data);
-            }
-        }
-    };
-    
-} // namespace pixelator
+#endif /* PIXELATOR_STB_IMAGE_DATA_VIEW_HPP */
